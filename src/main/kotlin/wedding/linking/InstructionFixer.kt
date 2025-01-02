c52f1237c764f82ac26ae1108cc7fcc3ba4a3d23
Luis Felger <firefligher.tornado@gmail.com>
2025-01-02 13:55:59 +0100

    Fix more instructions

--- src/main/kotlin/wedding/linking/InstructionFixer.kt
+++ src/main/kotlin/wedding/linking/InstructionFixer.kt
@@ -4,10 +4,79 @@ import dev.fir3.iwan.io.wasm.models.Expression
 import dev.fir3.iwan.io.wasm.models.instructions.*
 import dev.fir3.wedding.linking.model.MutableRelocationContainer
 import dev.fir3.wedding.linking.model.RelocationTablesContainer
+import dev.fir3.wedding.linking.model.data.ActiveRelocatedData
+import dev.fir3.wedding.linking.model.data.PassiveRelocatedData
+import dev.fir3.wedding.linking.model.element.ActiveRelocatedElement
+import dev.fir3.wedding.linking.model.element.DeclarativeRelocatedElement
+import dev.fir3.wedding.linking.model.element.PassiveRelocatedElement
 import dev.fir3.wedding.linking.model.function.DefinedRelocatedFunction
 import dev.fir3.wedding.linking.model.function.ImportedRelocatedFunction
+import dev.fir3.wedding.linking.model.global.DefinedRelocatedGlobal
+import dev.fir3.wedding.linking.model.global.ImportedRelocatedGlobal
 
 internal fun fixInstructions(container: MutableRelocationContainer) {
+    // Data
+
+    val fixedDatas = container.datas.map { data ->
+        when (data) {
+            is ActiveRelocatedData -> data.copy(
+                offset = fixExpression(
+                    data.offset,
+                    data.originalModule,
+                    container
+                )
+            )
+            is PassiveRelocatedData -> data
+        }
+    }
+
+    container.datas.clear()
+    container.datas.addAll(fixedDatas)
+
+    // Elements
+
+    val fixedElements = container.elements.map { element ->
+        when (element) {
+            is ActiveRelocatedElement -> element.copy(
+                initializers = element.initializers.map { initializer ->
+                    fixExpression(
+                        initializer,
+                        element.originalModule,
+                        container
+                    )
+                },
+                offset = fixExpression(
+                    element.offset,
+                    element.originalModule,
+                    container
+                )
+            )
+            is DeclarativeRelocatedElement -> element.copy(
+                initializers = element.initializers.map { initializer ->
+                    fixExpression(
+                        initializer,
+                        element.originalModule,
+                        container
+                    )
+                }
+            )
+            is PassiveRelocatedElement -> element.copy(
+                initializers = element.initializers.map { initializer ->
+                    fixExpression(
+                        initializer,
+                        element.originalModule,
+                        container
+                    )
+                }
+            )
+        }
+    }
+
+    container.elements.clear()
+    container.elements.addAll(fixedElements)
+
+    // Functions
+
     val fixedFunctions = container.functions.map { function ->
         when (function) {
             is DefinedRelocatedFunction -> function.copy(
@@ -24,6 +93,24 @@ internal fun fixInstructions(container: MutableRelocationContainer) {
 
     container.functions.clear()
     container.functions.addAll(fixedFunctions)
+
+    // Globals
+
+    val fixedGlobals = container.globals.map { global ->
+        when (global) {
+            is DefinedRelocatedGlobal -> global.copy(
+                initializer = fixExpression(
+                    global.initializer,
+                    global.originalModule,
+                    container
+                )
+            )
+            is ImportedRelocatedGlobal -> global
+        }
+    }
+
+    container.globals.clear()
+    container.globals.addAll(fixedGlobals)
 }
 
 private fun fixExpression(
