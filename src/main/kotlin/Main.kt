e40c8a4b9524cf12070bc3006adc783262d29e42
Luis Felger <firefligher.tornado@gmail.com>
2025-01-12 22:22:33 +0100

    Quicksave

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,11 @@ import dev.fir3.wedding.linker.fixing.fixElements
 import dev.fir3.wedding.linker.fixing.fixFunctions
 import dev.fir3.wedding.linker.fixing.fixGlobals
 import dev.fir3.wedding.linker.linking.link
+import dev.fir3.wedding.linker.linking.linkStartFunctions
 import dev.fir3.wedding.linker.merging.*
 import dev.fir3.wedding.linker.pool.*
+import dev.fir3.wedding.linker.pool.Data
+import dev.fir3.wedding.linker.pool.FunctionType
 import dev.fir3.wedding.linker.relocation.*
 import dev.fir3.wedding.linker.renaming.rename
 import dev.fir3.wedding.linker.renaming.renameImport
@@ -18,6 +21,7 @@ import dev.fir3.wedding.linker.sanity.checkForDuplicateExports
 import dev.fir3.wedding.wasm.*
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.util.UUID
 import kotlin.system.exitProcess
 
 private fun readModule(path: String): Module = FileInputStream(path)
@@ -46,6 +50,7 @@ private fun linkModules(
         exitProcess(1)
     }
 
+    pool.linkStartFunctions(UUID.randomUUID().toString())
     val conflicts = pool.link()
 
     if (conflicts.isNotEmpty()) {
@@ -77,174 +82,285 @@ private fun linkModules(
 }
 
 fun main(args: Array<String>) {
-    // Test Scenario
+    val modulesPaths = mapOf(
+        "FStar" to "input/FStar.wasm",
+        "Hacl_Bignum25519_51" to "input/Hacl_Bignum25519_51.wasm",
+        "Hacl_Curve25519_51" to "input/Hacl_Curve25519_51.wasm",
+        "Hacl_Ed25519" to "input/Hacl_Ed25519.wasm",
+        "Hacl_Ed25519_PrecompTable" to "input/Hacl_Ed25519_PrecompTable.wasm",
+        "Hacl_Hash_SHA2" to "input/Hacl_Hash_SHA2.wasm",
+        "Polyfill" to "polyfill/Polyfill.wasm",
+        "WasmSupport" to "input/WasmSupport.wasm"
+    )
 
-    val moduleFStar = readModule("input/FStar.wasm")
-    val moduleHaclHashMd5 = readModule("input/Hacl_Hash_MD5.wasm")
-    val moduleWasmSupport = readModule("input/WasmSupport.wasm")
-    val moduleWasmSupportGenerated = readModule("input/WasmSupport.generated.wasm")
-
-    val moduleWasmSupportJoined = linkModules(
-        mapOf(
-            "WasmSupport" to moduleWasmSupport,
-            "WasmSupport.Generated" to moduleWasmSupportGenerated
-        )
-    ) { pool ->
-        pool.renameImportModule(
-            ImportIdentifier(
-                module = "WasmSupport",
-                name = "WasmSupport_malloc",
-                sourceModule = "WasmSupport"
-            ),
-            "WasmSupport.Generated"
-        )
-
-        pool.renameImportModule(
-            ImportIdentifier(
-                module = "WasmSupport",
-                name = "WasmSupport_trap",
-                sourceModule = "WasmSupport"
-            ),
-            "WasmSupport.Generated"
-        )
+    val modules = modulesPaths.mapValues { (_, path) ->
+        readModule(path)
     }
 
-    val moduleLinked = linkModules(
-        mapOf(
-            "FStar" to moduleFStar,
-            "Hacl_Hash_MD5" to moduleHaclHashMd5,
-            "WasmSupport" to moduleWasmSupportJoined,
-        )
-    ) { pool ->
-        pool.rename(
-            ExportIdentifier(
-                module = "FStar",
-                names = setOf("data_size")
-            ),
-            "FStar_data_size"
-        )
+    val linkedModule = linkModules(modules) { pool ->
+        // Support module for the polyfill
 
-        pool.renameImport(
-            ImportIdentifier(
-                module = "FStar",
-                name = "data_start",
-                sourceModule = "Karamel"
-            ),
-            "FStar_data_start"
-        )
+        val supportModule = UUID.randomUUID().toString()
 
-        pool.rename(
-            ExportIdentifier(
-                module = "Hacl_Hash_MD5",
-                names = setOf("data_size")
-            ),
-            "Hacl_Hash_MD5_data_size"
-        )
-
-        pool.renameImport(
-            ImportIdentifier(
-                module = "Hacl_Hash_MD5",
-                name = "data_start",
-                sourceModule = "Karamel"
-            ),
-            "Hacl_Hash_MD5_data_start"
-        )
-
-        pool.rename(
-            ExportIdentifier(
-                module = "WasmSupport",
-                names = setOf("data_size")
-            ),
-            "WasmSupport_data_size"
-        )
-
-        pool.renameImport(
-            ImportIdentifier(
-                module = "WasmSupport",
-                name = "data_start",
-                sourceModule = "Karamel"
-            ),
-            "WasmSupport_data_start"
-        )
-
-        pool.renameImportModule(
-            ImportIdentifier(
-                module = "WasmSupport",
-                name = "Hacl_Hash_MD5_hash",
-                sourceModule = "env"
-            ),
-            "Hacl_Hash_MD5"
-        )
-    }
-
-    val moduleLinked3 = linkModules(
-        mapOf(
-            "LINKED" to moduleLinked,
-            "Karamel" to Module(
-                emptyList(),
-                listOf(
-                    ActiveData(
-                        ubyteArrayOf(0xC8u).toByteArray(),
-                        0u,
-                        listOf(Int32ConstInstruction(0))
+        pool.add(
+            FunctionType(
+                mutableMapOf(
+                    SourceModule::class to SourceModule(supportModule),
+                    SourceIndex::class to SourceIndex(0u),
+                    FunctionTypeInfo::class to FunctionTypeInfo(
+                        dev.fir3.wedding.wasm.FunctionType(
+                            listOf(NumberType.INT32),
+                            emptyList()
+                        )
                     )
-                ),
-                emptyList(),
-                listOf(
-                    GlobalExport(0u, "FStar_data_start"),
-                    GlobalExport(1u, "Hacl_Hash_MD5_data_start"),
-                    GlobalExport(2u, "WasmSupport_data_start")
-                ),
-                emptyList(),
-                listOf(
-                    dev.fir3.wedding.wasm.Global(
-                        listOf(Int32ConstInstruction(11024)),
-                        false,
-                        NumberType.INT32
-                    ),
-                    dev.fir3.wedding.wasm.Global(
-                        listOf(Int32ConstInstruction(12048)),
-                        false,
-                        NumberType.INT32
-                    ),
-                    dev.fir3.wedding.wasm.Global(
-                        listOf(Int32ConstInstruction(13072)),
-                        false,
-                        NumberType.INT32
-                    )
-                ),
-                listOf(
-                    MemoryImport(
-                        limits = Limits(256u, 256u),
-                        module = "LINKED",
-                        name = "memory"
-                    )
-                ),
-                emptyList(),
-                null,
-                emptyList(),
-                emptyList()
+                )
             )
         )
-    ) { pool ->
-        pool.renameImport(
-            ImportIdentifier(
-                module = "LINKED",
-                name = "mem",
-                sourceModule = "Karamel"
+
+        pool.add(
+            Function(
+                mutableMapOf(
+                    SourceModule::class to SourceModule(supportModule),
+                    SourceIndex::class to SourceIndex(0u),
+                    FunctionTypeIndex::class to FunctionTypeIndex(0u),
+                    FunctionBody::class to FunctionBody(
+                        Code(
+                            listOf(
+                                Int32ConstInstruction(0),
+                                LocalGetInstruction(0u),
+                                Int32StoreInstruction(0u, 0u)
+                            ),
+                            emptyList()
+                        )
+                    ),
+                    AssignedName::class to
+                            AssignedName("Support_SetHaclStackPtr")
+                )
+            )
+        )
+
+        pool.add(
+            FunctionType(
+                mutableMapOf(
+                    SourceModule::class to SourceModule(supportModule),
+                    SourceIndex::class to SourceIndex(1u),
+                    FunctionTypeInfo::class to FunctionTypeInfo(
+                        dev.fir3.wedding.wasm.FunctionType(
+                            emptyList(),
+                            listOf(NumberType.INT32)
+                        )
+                    )
+                )
+            )
+        )
+
+        pool.add(
+            Function(
+                mutableMapOf(
+                    SourceModule::class to SourceModule(supportModule),
+                    SourceIndex::class to SourceIndex(1u),
+                    FunctionTypeIndex::class to FunctionTypeIndex(1u),
+                    FunctionBody::class to FunctionBody(
+                        Code(
+                            listOf(
+                                Int32ConstInstruction(0),
+                                Int32LoadInstruction(0u, 0u)
+                            ),
+                            emptyList()
+                        )
+                    ),
+                    AssignedName::class to
+                            AssignedName("Support_GetHaclStackPtr")
+                )
+            )
+        )
+
+        // Polyfill modifications.
+
+        pool.renameImportModule(
+            identifier = ImportIdentifier(
+                module = "Polyfill",
+                name = "Hacl_Hash_SHA2_hash_256",
+                sourceModule = "env"
             ),
-            "memory"
+            newModule = "Hacl_Hash_SHA2"
         )
 
         pool.renameImportModule(
-            ImportIdentifier(
-                module = "LINKED",
-                name = "memory",
-                sourceModule = "Karamel"
+            identifier = ImportIdentifier(
+                module = "Polyfill",
+                name = "Hacl_Hash_SHA2_hash_512",
+                sourceModule = "env"
             ),
-            "LINKED"
+            newModule = "Hacl_Hash_SHA2"
+        )
+
+        pool.renameImportModule(
+            identifier = ImportIdentifier(
+                module = "Polyfill",
+                name = "Hacl_Ed25519_sign",
+                sourceModule = "env"
+            ),
+            newModule = "Hacl_Ed25519"
+        )
+
+        pool.renameImportModule(
+            identifier = ImportIdentifier(
+                module = "Polyfill",
+                name = "Support_GetHaclStackPtr",
+                sourceModule = "env"
+            ),
+            newModule = supportModule
+        )
+
+        pool.renameImportModule(
+            identifier = ImportIdentifier(
+                module = "Polyfill",
+                name = "Support_SetHaclStackPtr",
+                sourceModule = "env"
+            ),
+            newModule = supportModule
+        )
+
+        // Some other code.
+
+        var dataOffset = 65536u + 128u
+
+        modules.keys.forEach { moduleName ->
+            if (moduleName == "Polyfill") return@forEach
+
+            // Rename things.
+
+            pool.rename(
+                identifier = ExportIdentifier(
+                    module = moduleName,
+                    names = setOf("data_size")
+                ),
+                newName = "R${moduleName}_data_size"
+            )
+
+            pool.renameImportModule(
+                identifier = ImportIdentifier(
+                    module = moduleName,
+                    name = "WasmSupport_malloc",
+                    sourceModule = "WasmSupport"
+                ),
+                newModule = "Polyfill"
+            )
+
+            pool.renameImportModule(
+                identifier = ImportIdentifier(
+                    module = moduleName,
+                    name = "WasmSupport_trap",
+                    sourceModule = "WasmSupport"
+                ),
+                newModule = "Polyfill"
+            )
+
+            pool.renameImport(
+                identifier = ImportIdentifier(
+                    module = moduleName,
+                    name = "mem",
+                    sourceModule = "Karamel"
+                ),
+                newName = "memory"
+            )
+
+            pool.renameImportModule(
+                identifier = ImportIdentifier(
+                    module = moduleName,
+                    name = "memory",
+                    sourceModule = "Karamel"
+                ),
+                newModule = "Polyfill"
+            )
+
+            // Fix Memory Layout.
+
+            val dataSizeGlobal = pool.resolve(
+                ExportIdentifier(
+                    module = moduleName,
+                    names = setOf("R${moduleName}_data_size"),
+                )
+            )
+
+            val dataSizeInstruction =
+                dataSizeGlobal!![GlobalInitializer::class]!!
+                    .instructions
+                    .single() as Int32ConstInstruction
+
+            val dataStartGlobal = pool.resolve(
+                identifier = ImportIdentifier(
+                    module = moduleName,
+                    name = "data_start",
+                    sourceModule = "Karamel"
+                ),
+            )
+
+            setOf(
+                AssignedImportModule::class,
+                AssignedImportName::class,
+                ImportDuplicate::class,
+                ImportModule::class,
+                ImportName::class,
+                ImportResolution::class
+            ).forEach {
+                dataStartGlobal!!.annotations.remove(it)
+            }
+
+            dataStartGlobal!![GlobalInitializer::class] = GlobalInitializer(
+                instructions = listOf(
+                    Int32ConstInstruction(dataOffset.toInt())
+                )
+            )
+
+            dataStartGlobal[AssignedName::class] =
+                AssignedName("${moduleName}_data_start")
+
+            dataOffset += dataSizeInstruction.constant.toUInt()
+        }
+
+        // Add some data that initializes the HACL*'s memory header.
+
+        val headerModule = UUID.randomUUID().toString()
+
+        pool.add(
+            Memory(
+                mutableMapOf(
+                    SourceModule::class to SourceModule(headerModule),
+                    ImportModule::class to ImportModule("Polyfill"),
+                    ImportName::class to ImportName("memory"),
+                    SourceIndex::class to SourceIndex(0u),
+                    MemoryInfo::class to MemoryInfo(
+                        Limits(1u, null)
+                    )
+                )
+            )
+        )
+
+        pool.add(
+            Data(
+                mutableMapOf(
+                    SourceModule::class to SourceModule(headerModule),
+                    SourceIndex::class to SourceIndex(0u),
+                    DataContent::class to DataContent(
+                        byteArrayOf(
+                            (dataOffset and 0xFFu).toByte(),
+                            ((dataOffset shr 8) and 0xFFu).toByte(),
+                            ((dataOffset shr 16) and 0xFFu).toByte(),
+                            ((dataOffset shr 24) and 0xFFu).toByte()
+                        )
+                    ),
+                    ActiveDataInfo::class to ActiveDataInfo(
+                        memoryIndex = 0u,
+                        offset = listOf(
+                            Int32ConstInstruction(0)
+                        )
+                    )
+                )
+            )
         )
     }
 
-    writeModule("LINKED.wasm", moduleLinked3)
+    writeModule("LINKED.wasm", linkedModule)
 }
